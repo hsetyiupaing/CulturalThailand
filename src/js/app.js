88b6596ae65c64033f318f002e9c0287c8bdd370
hset7124 <hsetyiup@gmail.com>
2025-11-22 18:18:26 +0700

    created header component

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,11 @@
 import { HeroBenner } from "./components/HeroBenner.js";
 import { InfoCards, MediaContent } from "./components/mediaComponents.js";
 import { ContentCard } from "./components/ContentCard.js";
+import { Header } from "./components/Header.js";
 
 customElements.define('hero-banner', HeroBenner);
 customElements.define('info-cards', InfoCards);
 customElements.define('content-card', ContentCard);
 customElements.define('media-content', MediaContent);
+
+customElements.define('header-bar', Header);
